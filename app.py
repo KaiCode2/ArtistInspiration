763e2a6710172259a54e8337b544ba296db00c37
Kai Aldag <kaialdag@icloud.com>
2016-01-24 00:26:42 -0800

    added custom routes

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ def index():
     c = Concept()
     return render_template("index.html", inspiration=c.conceptualize())
 
+@app.route("/<string:subject>/<string:descriptor>/<string:setting>/")
+def concept_route(subject, descriptor, setting):
+    print subject, descriptor, setting
+    c = Concept([subject, descriptor, setting])
+    return render_template("index.html", inspiration=c.conceptualize())
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
